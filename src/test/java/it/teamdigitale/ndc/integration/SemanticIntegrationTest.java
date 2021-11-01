7fb46e57a2b44e6a2e8e60d42d9290d189e912fe
Ashay Thorat <ash1425@gmail.com>
2021-11-01 11:57:54 +0530

    updates the test to add and then verify the data

--- src/test/java/it/teamdigitale/ndc/integration/SemanticIntegrationTest.java
+++ src/test/java/it/teamdigitale/ndc/integration/SemanticIntegrationTest.java
@@ -3,38 +3,55 @@ package it.teamdigitale.ndc.integration;
 import static org.testcontainers.utility.DockerImageName.parse;
 
 import org.apache.jena.arq.querybuilder.SelectBuilder;
+import org.apache.jena.arq.querybuilder.UpdateBuilder;
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.ResultSet;
+import org.apache.jena.update.UpdateExecutionFactory;
+import org.apache.jena.update.UpdateRequest;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 public class SemanticIntegrationTest {
 
-    @Container
     private static GenericContainer virtuoso = new GenericContainer(parse("tenforce/virtuoso"))
         .withReuse(true)
-        .withExposedPorts(8890);
+        .withExposedPorts(8890)
+        .withEnv("DBA_PASSWORD", "dba")
+        .withEnv("SPARQL_UPDATE", "true");
+
+    @BeforeAll
+    static void setup() {
+        virtuoso.start();
+    }
 
     @Test
     void shouldExecuteSparqlOnVirtuosoTestcontainer() {
+        // given
         String sparqlUrl = "http://localhost:" + virtuoso.getMappedPort(8890) + "/sparql";
+        insertSampleData(sparqlUrl);
 
-        Query selectQuery = new SelectBuilder()
-            .addVar("?s").addVar("?p").addVar("?o")
-            .addWhere("?s", "?p", "?o")
-            .setLimit(10)
+        // when
+        Query findTitle = new SelectBuilder()
+            .addVar("o")
+            .addWhere("<http://example/egbook>", "<http://example/title>", "?o")
+            .from("http://example")
             .build();
-
         ResultSet resultSet =
-            QueryExecutionFactory.sparqlService(sparqlUrl, selectQuery)
-                .execSelect();
+            QueryExecutionFactory.sparqlService(sparqlUrl, findTitle).execSelect();
 
-        resultSet.forEachRemaining(result -> {
-            System.out.println(result.get("s") + " " + result.get("p") + " " + result.get("o"));
-        });
+        // then
+        assert resultSet.next().get("o").toString().equals("This is an example title");
+    }
+
+    private void insertSampleData(String sparqlUrl) {
+        UpdateRequest updateRequest = new UpdateBuilder()
+            .addInsert("<http://example>", "<http://example/egbook>", "<http://example/title>",
+                "This is an example title")
+            .buildRequest();
+        UpdateExecutionFactory.createRemote(updateRequest, sparqlUrl).execute();
     }
 }
