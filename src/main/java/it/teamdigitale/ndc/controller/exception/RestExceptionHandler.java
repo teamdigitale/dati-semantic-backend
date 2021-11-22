f81a1b0ed48b7505b40c31a672d2a031bfda2e60
keerthana.r1 <keerthana.r@thoughtworks.com>
2021-11-22 11:56:12 +0530

    Consolidated all rest exception handler in single class
    co-authored-by: Ashay thorat <ashayt@thoughtworks.com>

--- src/main/java/it/teamdigitale/ndc/controller/exception/RestExceptionHandler.java
+++ src/main/java/it/teamdigitale/ndc/controller/exception/RestExceptionHandler.java
@@ -3,6 +3,7 @@ package it.teamdigitale.ndc.controller.exception;
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import javax.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -19,5 +20,14 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         body.put("message", exception.getMessage());
 
         return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = {ConstraintViolationException.class})
+    protected ResponseEntity<Object> handleValidationFailures(RuntimeException ex) {
+        String errorMessage = "Validation for parameter failed " + ex.getMessage();
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", errorMessage);
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 }
--- src/test/java/it/teamdigitale/ndc/controller/exception/RestExceptionHandlerTest.java
+++ src/test/java/it/teamdigitale/ndc/controller/exception/RestExceptionHandlerTest.java
@@ -1,9 +1,9 @@
 package it.teamdigitale.ndc.controller.exception;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import java.util.Map;
-import org.assertj.core.api.Assertions;
+import java.util.Set;
+import javax.validation.ConstraintViolationException;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,14 +14,28 @@ class RestExceptionHandlerTest {
     void shouldHandleVocabularyNotFound() {
         RestExceptionHandler handler = new RestExceptionHandler();
         ResponseEntity<Object> responseEntity =
-            handler.handleNotFound(new VocabularyDataNotFoundException("testIndex"));
+                handler.handleNotFound(new VocabularyDataNotFoundException("testIndex"));
 
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
         Object body = responseEntity.getBody();
         assertThat(body).isInstanceOf(Map.class);
         assertThat(body).isNotNull();
         Map<String, String> responseMap = (Map<String, String>) body;
-        Assertions.assertThat(responseMap.get("message"))
-            .isEqualTo("Unable to find vocabulary data for : testIndex");
+        assertThat(responseMap.get("message"))
+                .isEqualTo("Unable to find vocabulary data for : testIndex");
+    }
+
+    @Test
+    void shouldHandleValidationException() {
+        RestExceptionHandler handler = new RestExceptionHandler();
+        ResponseEntity<Object> responseEntity =
+                handler.handleValidationFailures(new ConstraintViolationException("1234", Set.of()));
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        Object body = responseEntity.getBody();
+        assertThat(body).isInstanceOf(Map.class);
+        assertThat(body).isNotNull();
+        Map<String, String> responseMap = (Map<String, String>) body;
+        assertThat(responseMap.get("message")).isEqualTo("Validation for parameter failed 1234");
     }
 }
