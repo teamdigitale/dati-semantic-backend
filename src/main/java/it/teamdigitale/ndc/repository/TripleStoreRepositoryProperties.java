dd1589a1db1229e50edfa0926daa1e30f0cc186d
Ginette Vellera <ginettv@thoughtworks.com>
2021-11-23 12:23:14 +0530

    29 - adding virtuoso properties for tests to work

--- src/main/java/it/teamdigitale/ndc/repository/TripleStoreRepositoryProperties.java
+++ src/main/java/it/teamdigitale/ndc/repository/TripleStoreRepositoryProperties.java
@@ -1,14 +1,22 @@
 package it.teamdigitale.ndc.repository;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Data
 @ConfigurationProperties(prefix = "virtuoso")
-@Component
+@Configuration
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties("$$beanFactory")
 public class TripleStoreRepositoryProperties {
-    private final String url;
-    private final String username;
-    private final String password;
+    private String url;
+    private String username;
+    private String password;
 }
