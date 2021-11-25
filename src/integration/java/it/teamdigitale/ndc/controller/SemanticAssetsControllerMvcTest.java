4a83e5673f4cf412f9f893f609b58007cc69b0f7
Ashay Thorat <ash1425@gmail.com>
2021-11-25 23:46:00 +0530

    #75 - verify reponse Dto in mockMvc tests

--- src/integration/java/it/teamdigitale/ndc/controller/SemanticAssetsControllerMvcTest.java
+++ src/integration/java/it/teamdigitale/ndc/controller/SemanticAssetsControllerMvcTest.java
@@ -1,5 +1,6 @@
 package it.teamdigitale.ndc.controller;
 
+import static it.teamdigitale.ndc.harvester.SemanticAssetType.CONTROLLED_VOCABULARY;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -13,6 +14,7 @@ import it.teamdigitale.ndc.controller.dto.SemanticAssetSearchResult;
 import it.teamdigitale.ndc.controller.dto.SemanticAssetsSearchDto;
 import it.teamdigitale.ndc.controller.exception.SemanticAssetNotFoundException;
 import it.teamdigitale.ndc.service.SemanticAssetSearchService;
+import java.time.LocalDate;
 import org.elasticsearch.common.collect.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,13 +36,59 @@ public class SemanticAssetsControllerMvcTest {
     void shouldFindByIri() throws Exception {
         when(searchService.findByIri("some-iri")).thenReturn(SemanticAssetDetailsDto.builder()
             .iri("some-iri")
+            .title("some-title")
+            .description("some-description")
+            .modified(LocalDate.parse("2020-01-01"))
+            .type(CONTROLLED_VOCABULARY)
+            .accrualPeriodicity("some-accrual-periodicity")
+            .conformsTo(List.of("some-conforms-to", "some-other-conforms-to"))
+            .contactPoint("some-contact-point")
+            .creator(List.of("some-creator", "some-other-creator"))
+            .distribution(List.of("some-distribution", "some-other-distribution"))
+            .issued(LocalDate.parse("2020-01-02"))
+            .endpointUrl("some-endpoint-url")
+            .keyConcept("some-key-concept")
+            .language(List.of("some-language", "some-other-language"))
+            .publisher(List.of("some-publisher", "some-other-publisher"))
+            .subject(List.of("some-subject", "some-other-subject"))
+            .temporal("some-temporal")
+            .versionInfo("some-version-info")
             .build());
 
         mockMvc.perform(get("/semantic-assets/details").param("iri", "some-iri"))
             .andDo(print())
             .andExpect(status().isOk())
             .andExpect(content().contentType("application/json"))
-            .andExpect(content().json("{\"iri\":\"some-iri\"}"));
+            .andExpect(jsonPath("$.iri").value("some-iri"))
+            .andExpect(jsonPath("$.title").value("some-title"))
+            .andExpect(jsonPath("$.description").value("some-description"))
+            .andExpect(jsonPath("$.modified").value("2020-01-01"))
+            .andExpect(jsonPath("$.type").value("CONTROLLED_VOCABULARY"))
+            .andExpect(jsonPath("$.accrualPeriodicity").value("some-accrual-periodicity"))
+            .andExpect(jsonPath("$.conformsTo").isArray())
+            .andExpect(jsonPath("$.conformsTo[0]").value("some-conforms-to"))
+            .andExpect(jsonPath("$.conformsTo[1]").value("some-other-conforms-to"))
+            .andExpect(jsonPath("$.contactPoint").value("some-contact-point"))
+            .andExpect(jsonPath("$.creator").isArray())
+            .andExpect(jsonPath("$.creator[0]").value("some-creator"))
+            .andExpect(jsonPath("$.creator[1]").value("some-other-creator"))
+            .andExpect(jsonPath("$.distribution").isArray())
+            .andExpect(jsonPath("$.distribution[0]").value("some-distribution"))
+            .andExpect(jsonPath("$.distribution[1]").value("some-other-distribution"))
+            .andExpect(jsonPath("$.issued").value("2020-01-02"))
+            .andExpect(jsonPath("$.endpointUrl").value("some-endpoint-url"))
+            .andExpect(jsonPath("$.keyConcept").value("some-key-concept"))
+            .andExpect(jsonPath("$.language").isArray())
+            .andExpect(jsonPath("$.language[0]").value("some-language"))
+            .andExpect(jsonPath("$.language[1]").value("some-other-language"))
+            .andExpect(jsonPath("$.publisher").isArray())
+            .andExpect(jsonPath("$.publisher[0]").value("some-publisher"))
+            .andExpect(jsonPath("$.publisher[1]").value("some-other-publisher"))
+            .andExpect(jsonPath("$.subject").isArray())
+            .andExpect(jsonPath("$.subject[0]").value("some-subject"))
+            .andExpect(jsonPath("$.subject[1]").value("some-other-subject"))
+            .andExpect(jsonPath("$.temporal").value("some-temporal"))
+            .andExpect(jsonPath("$.versionInfo").value("some-version-info"));
     }
 
     @Test
@@ -62,7 +110,13 @@ public class SemanticAssetsControllerMvcTest {
                 .pageNumber(1)
                 .totalPages(5)
                 .data(List.of(SemanticAssetsSearchDto.builder()
+                    .iri("some-iri")
+                    .description("some-description")
+                    .modified(LocalDate.parse("2020-01-01"))
+                    .rightsHolder("some-rights-holder")
+                    .theme(List.of("study", "sports"))
                     .title("searchText contains")
+                    .type(CONTROLLED_VOCABULARY)
                     .build()))
                 .build());
 
@@ -72,7 +126,14 @@ public class SemanticAssetsControllerMvcTest {
             .andExpect(content().contentType("application/json"))
             .andExpect(jsonPath("$.pageNumber").value(1))
             .andExpect(jsonPath("$.totalPages").value(5))
-            .andExpect(jsonPath("$.data[0].title").value("searchText contains"));
+            .andExpect(jsonPath("$.data[0].iri").value("some-iri"))
+            .andExpect(jsonPath("$.data[0].description").value("some-description"))
+            .andExpect(jsonPath("$.data[0].modified").value("2020-01-01"))
+            .andExpect(jsonPath("$.data[0].rightsHolder").value("some-rights-holder"))
+            .andExpect(jsonPath("$.data[0].theme[0]").value("study"))
+            .andExpect(jsonPath("$.data[0].theme[1]").value("sports"))
+            .andExpect(jsonPath("$.data[0].title").value("searchText contains"))
+            .andExpect(jsonPath("$.data[0].type").value("CONTROLLED_VOCABULARY"));
     }
 
     @Test
