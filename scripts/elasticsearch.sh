c80aebffb06625edc1978a7c0c46e859e2b964c1
Ashay Thorat <ash1425@gmail.com>
2021-11-29 15:23:31 +0530

    #tech - restricting local elasticsearch so that it leaves resources for other containers too.
    Co-authored-by: Keerthana R <keerthana.r@thoughtworks.com>

--- scripts/elasticsearch.sh
+++ scripts/elasticsearch.sh
@@ -6,4 +6,7 @@ docker >/dev/null 2>&1 || echo "Docker must be installed"
 
 docker run -p 9200:9200 \
   -e "discovery.type=single-node" \
+  -e "ES_JAVA_OPTS=-Xms512m -Xmx512m" \
+  --cpus 0.8 \
+  --memory 600Mi \
   -d docker.elastic.co/elasticsearch/elasticsearch:7.12.0
