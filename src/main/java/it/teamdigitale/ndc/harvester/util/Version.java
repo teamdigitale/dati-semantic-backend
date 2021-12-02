7e0ebc09531bb8e33bc4e9d7bc3e4a190956784b
Rony Cesana <rony.cesana@thoughtworks.com>
2021-12-02 15:12:54 +0100

    32 - Avoid throwing when building Version objects
    If a string does not represent a version, do not throw an exception, but return an empty Optional.

---
+++ src/main/java/it/teamdigitale/ndc/harvester/util/Version.java
@@ -0,0 +1,96 @@
+package it.teamdigitale.ndc.harvester.util;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.net.SSLUtilBase;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@RequiredArgsConstructor
+public class Version implements Comparable<Version> {
+    private static final Pattern FULL_PATTERN = Pattern.compile("v?(\\d+)((?:\\.\\d+)*)");
+    private static final Pattern SUB_VERSIONS_PATTERN = Pattern.compile("\\.(\\d+)");
+    private final String sourceString;
+    private final boolean isLatest;
+    private final List<Integer> tokens;
+
+    @Override
+    public String toString() {
+        return sourceString;
+    }
+
+    @Override
+    public int compareTo(Version o) {
+        if (isLatest) {
+            if (o.isLatest) {
+                return 0;
+            }
+            return 1;
+        }
+        if (o.isLatest) {
+            return -1;
+        }
+
+        int otherSize = o.tokens.size();
+        int mySize = tokens.size();
+        for (int i = 0; i < mySize && i < otherSize; i++) {
+            int comparison = Integer.compare(tokens.get(i), o.tokens.get(i));
+            if (comparison != 0) {
+                return comparison;
+            }
+        }
+
+        // if the common parts of token list are the same, the version with more tokens is subsequent (e.g. 1.2.4 > 1.2)
+        return Integer.compare(mySize, otherSize);
+    }
+
+    public static Optional<Version> of(String s) {
+        if (s == null) {
+            throw new IllegalArgumentException("Please provide a valid version string");
+        }
+
+        if ("latest".equals(s)) {
+            return Optional.of(new Version(s, true, Collections.emptyList()));
+        }
+
+        Matcher matcher = FULL_PATTERN.matcher(s);
+        if (!matcher.matches()) {
+            return Optional.empty();
+        }
+
+        List<Integer> tokens = new ArrayList<>();
+        tokens.add(Integer.valueOf(matcher.group(1)));
+
+        String subVersions = matcher.group(2);
+        if (subVersions != null) {
+            matcher = SUB_VERSIONS_PATTERN.matcher(subVersions);
+            while (matcher.find()) {
+                tokens.add(Integer.valueOf(matcher.group(1)));
+            }
+        }
+
+        return Optional.of(new Version(s, false, tokens));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Version version = (Version) o;
+        return sourceString.equals(version.sourceString);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sourceString);
+    }
+}
---
+++ src/test/java/it/teamdigitale/ndc/harvester/util/VersionTest.java
@@ -0,0 +1,94 @@
+package it.teamdigitale.ndc.harvester.util;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Optional;
+import java.util.function.BiConsumer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class VersionTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"latest", "1", "v1", "1.2.3", "v1.2.4"})
+    void canCreateValidVersion(String version) {
+        assertThat(Version.of(version)).isNotEmpty();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"later", "old", "v1.", "1.", "1.alpha"})
+    void createsEmptyVersionsForInvalidVersion(String version) {
+        assertThat(Version.of(version)).isEmpty();
+    }
+
+    @Test
+    void cannotCreateFromNull() {
+        assertThatThrownBy(() -> Version.of(null))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void canCreateFromLatest() {
+        Version.of("latest");
+    }
+
+    @Test
+    void compareLatestBefore() {
+        assertFirstVersionIsGreaterThanSecond("latest", "0.1");
+    }
+
+    @Test
+    void compareLatestAfter() {
+        assertFirstVersionIsLessThanSecond("0.2", "latest");
+    }
+
+    @Test
+    void compareSingleNumbers() {
+        assertFirstVersionIsGreaterThanSecond("2", "1");
+    }
+
+    @Test
+    void compareSingleNumbersWithLeadingV() {
+        assertFirstVersionIsGreaterThanSecond("v2", "v1");
+    }
+
+    @Test
+    void compareNestedNumbers() {
+        assertFirstVersionIsLessThanSecond("1.2.3", "1.3.4");
+    }
+
+    @Test
+    void compareVersionWithItsSubVersion() {
+        assertFirstVersionIsLessThanSecond("1.2", "1.2.4");
+    }
+
+    @Test
+    void compareSubVersionWithItsSupVersion() {
+        assertFirstVersionIsGreaterThanSecond("1.2.4", "1.2");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"latest", "1", "v1", "1.2.3", "v1.2.4"})
+    void compareVersionWithItself(String version) {
+        assertValidVersionsAndCompare(version, version, (v1, v2) -> assertThat(v1).isEqualByComparingTo(v2));
+    }
+
+    private void assertFirstVersionIsGreaterThanSecond(String a, String b) {
+        assertValidVersionsAndCompare(a, b, (v1, v2) -> assertThat(v1).isGreaterThan(v2));
+    }
+
+    private void assertFirstVersionIsLessThanSecond(String s1, String s2) {
+        assertValidVersionsAndCompare(s1, s2, (v1, v2) -> assertThat(v1).isLessThan(v2));
+    }
+
+    private void assertValidVersionsAndCompare(String firstVersionSource, String secondVersionSource, BiConsumer<Version, Version> assertionOnVersion) {
+        Optional<Version> firstVersion = Version.of(firstVersionSource);
+        Optional<Version> secondVersion = Version.of(secondVersionSource);
+        assertThat(firstVersion).isNotEmpty();
+        assertThat(secondVersion).isNotEmpty();
+        assertionOnVersion.accept(firstVersion.get(), secondVersion.get());
+    }
+}
