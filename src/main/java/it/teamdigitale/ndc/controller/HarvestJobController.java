e96a907b1692b610bb556ee674fb7e875b5bc0e7
Ginette Vellera <ginettv@thoughtworks.com>
2021-12-02 16:05:20 +0530

    34 - fixing integration tests

--- src/main/java/it/teamdigitale/ndc/controller/HarvestJobController.java
+++ src/main/java/it/teamdigitale/ndc/controller/HarvestJobController.java
@@ -1,24 +1,36 @@
 package it.teamdigitale.ndc.controller;
 
-import lombok.RequiredArgsConstructor;
+import it.teamdigitale.ndc.harvester.HarvesterService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @Slf4j
 @RestController
 @RequestMapping
-@RequiredArgsConstructor
 public class HarvestJobController {
-    final JobLauncher jobLauncher;
-    final Job harvestSemanticAssetsJob;
+    private final HarvesterService harvesterService;
+    private final JobLauncher jobLauncher;
+    private final Job harvestSemanticAssetsJob;
 
-    @GetMapping("harvest/start")
+    @Autowired
+    public HarvestJobController(HarvesterService harvesterService, JobLauncher jobLauncher, Job harvestSemanticAssetsJob) {
+        this.harvesterService = harvesterService;
+        this.jobLauncher = jobLauncher;
+        this.harvestSemanticAssetsJob = harvestSemanticAssetsJob;
+    }
+
+    @PostMapping("harvest/start")
     public String startHarvestJob() {
         try {
             JobParameters jobParameters = new JobParametersBuilder()
@@ -26,7 +38,13 @@ public class HarvestJobController {
             jobLauncher.run(harvestSemanticAssetsJob, jobParameters);
         } catch (Exception e) {
             log.error(e.getMessage());
+            return "Harvest job has error " + e.getMessage();
         }
         return "Harvest job started";
+    }
+
+    @PostMapping("scheduler/harvester")
+    public void csv(@RequestParam("repoURI") String repoUri) throws IOException {
+        harvesterService.harvest(repoUri);
     }
 }
