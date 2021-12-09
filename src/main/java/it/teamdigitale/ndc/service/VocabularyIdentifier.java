5593cd9fa64e7d4c924b1787b87bb06abb6dac94
Rony Cesana <rony.cesana@thoughtworks.com>
2021-12-09 09:06:50 +0100

    33 - Add tests for VocabularyIdentifier

--- src/main/java/it/teamdigitale/ndc/service/VocabularyIdentifier.java
+++ src/main/java/it/teamdigitale/ndc/service/VocabularyIdentifier.java
@@ -1,20 +1,26 @@
 package it.teamdigitale.ndc.service;
 
 import lombok.EqualsAndHashCode;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @EqualsAndHashCode
 public class VocabularyIdentifier {
     private final String rightsHolder;
     private final String keyConcept;
 
+    private final String indexName;
+
+    public VocabularyIdentifier(String rightsHolder, String keyConcept) {
+        this.rightsHolder = rightsHolder;
+        this.keyConcept = keyConcept;
+        this.indexName = String.join(".", rightsHolder, keyConcept).toLowerCase();
+    }
+
     public String getIndexName() {
-        return String.join(".", rightsHolder, keyConcept).toLowerCase();
+        return indexName;
     }
 
     @Override
     public String toString() {
-        return getIndexName();
+        return indexName;
     }
 }
