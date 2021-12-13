bf98acbd55d775b8d821124539da9d576c40c023
Rony Cesana <rony.cesana@thoughtworks.com>
2021-12-13 09:02:49 +0100

    91 - Drop constraint for single ontology in folder

--- src/test/java/it/teamdigitale/ndc/harvester/scanners/OntologyFolderScannerTest.java
+++ src/test/java/it/teamdigitale/ndc/harvester/scanners/OntologyFolderScannerTest.java
@@ -1,21 +1,20 @@
 package it.teamdigitale.ndc.harvester.scanners;
 
 import it.teamdigitale.ndc.harvester.model.SemanticAssetPath;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ExtendWith(MockitoExtension.class)
 class OntologyFolderScannerTest extends BaseFolderScannerTest {
@@ -33,6 +32,13 @@ class OntologyFolderScannerTest extends BaseFolderScannerTest {
         List<SemanticAssetPath> ontologyPaths = scanner.scanFolder(folder);
 
         assertThat(ontologyPaths).containsOnly(SemanticAssetPath.of("onto.ttl"));
+    }
+
+    @Test
+    void shouldComplainForMissingProperties() {
+        assertThatThrownBy(() -> new OntologyFolderScanner(fileUtils, null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("properties");
     }
 
     @ParameterizedTest
@@ -56,11 +62,46 @@ class OntologyFolderScannerTest extends BaseFolderScannerTest {
     }
 
     @Test
-    void shouldIgnoreExamples() throws IOException {
+    void shouldIgnoreExamplesAndAligns() throws IOException {
         mockFolderToContain("onto.ttl", "examples.ttl", "aligns-onto.ttl", "onto-aligns.ttl");
 
         List<SemanticAssetPath> ontologyPaths = scanner.scanFolder(folder);
 
         assertThat(ontologyPaths).containsOnly(SemanticAssetPath.of("onto.ttl"));
+    }
+
+    @Test
+    void shouldPickMultipleTurtlesInSameFolder() throws IOException {
+        mockFolderToContain("onto1.ttl", "examples.ttl", "onto2.ttl");
+
+        List<SemanticAssetPath> ontologyPaths = scanner.scanFolder(folder);
+
+        assertThat(ontologyPaths).containsOnly(SemanticAssetPath.of("onto1.ttl"), SemanticAssetPath.of("onto2.ttl"));
+    }
+
+    @Test
+    void shouldSupportNullSkipWords() throws IOException {
+        scanWithSkipWords(null);
+    }
+
+    @Test
+    void shouldSupportEmptySkipWords() throws IOException {
+        scanWithSkipWords(Collections.emptyList());
+    }
+
+    @Test
+    void shouldComplainForShortSkipWords() throws IOException {
+        assertThatThrownBy(() -> new OntologyFolderScanner(fileUtils, OntologyFolderScannerProperties.forWords("example", "-")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("long");
+    }
+
+    private void scanWithSkipWords(List<String> skipWords) throws IOException {
+        OntologyFolderScanner scanner = new OntologyFolderScanner(fileUtils, new OntologyFolderScannerProperties(skipWords));
+        mockFolderToContain("example.ttl");
+
+        List<SemanticAssetPath> paths = scanner.scanFolder(folder);
+
+        assertThat(paths.contains(SemanticAssetPath.of("example.ttl")));
     }
 }
