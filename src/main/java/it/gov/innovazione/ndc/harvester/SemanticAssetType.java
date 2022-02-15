597b4835da2813a87e29dd0df5a8bf652b9717ca
Roberto Polli <robipolli@gmail.com>
2022-02-15 18:26:56 +0100

    Fix: #37. Support asset/ folders.

--- src/main/java/it/gov/innovazione/ndc/harvester/SemanticAssetType.java
+++ src/main/java/it/gov/innovazione/ndc/harvester/SemanticAssetType.java
@@ -5,27 +5,32 @@ import lombok.Getter;
 @Getter
 public enum SemanticAssetType {
     ONTOLOGY("ontology",
+            "assets/ontologies",
             "Ontologie",
             "http://www.w3.org/2002/07/owl#Ontology",
             true),
     CONTROLLED_VOCABULARY("controlled vocabulary",
+            "assets/vocabularies",
             "VocabolariControllati",
             "http://dati.gov.it/onto/dcatapit#Dataset",
             false),
     SCHEMA("schema",
+            "assets/schemas",
             "Schema",
             "http://dati.gov.it/onto/dcatapit#Dataset",
             false);
 
     private final String description;
     private final String folderName;
+    private final String legacyFolderName;
     private final String typeIri;
     private final boolean isIgnoringObsoleteVersions;
 
-    SemanticAssetType(String description, String folderName, String typeIri,
-                      boolean isIgnoringObsoleteVersions) {
+    SemanticAssetType(String description, String folderName, String legacyFolderName,
+                      String typeIri, boolean isIgnoringObsoleteVersions) {
         this.description = description;
         this.folderName = folderName;
+        this.legacyFolderName = legacyFolderName;
         this.typeIri = typeIri;
         this.isIgnoringObsoleteVersions = isIgnoringObsoleteVersions;
     }
