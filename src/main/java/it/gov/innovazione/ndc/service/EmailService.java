0b8e6e45c4f23b6fb0f92645745a99e81c35aced
ndc-dxc <giuseppe.nespolino@dxc.com>
2024-07-05 10:44:32 +0200

    added test code to debug sendmail

--- src/main/java/it/gov/innovazione/ndc/service/EmailService.java
+++ src/main/java/it/gov/innovazione/ndc/service/EmailService.java
@@ -1,18 +1,24 @@
 package it.gov.innovazione.ndc.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 class EmailService {
 
     private final JavaMailSender javaMailSender;
     @Value("${alerter.mail.sender}")
     private final String from;
+    @Value("${spring.mail.properties.mail.debug:false}")
+    private boolean mailDebug;
 
     void sendEmail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
@@ -21,6 +27,14 @@ class EmailService {
         message.setSubject(subject);
         message.setText(text);
         javaMailSender.send(message);
+    }
+
+    @EventListener(ApplicationStartedEvent.class)
+    void debugSendMail() {
+        if (mailDebug) {
+            log.info("Sending test email");
+            sendEmail("servicedesk-schema@istat.it", "Test", "Test");
+        }
     }
 
 }
