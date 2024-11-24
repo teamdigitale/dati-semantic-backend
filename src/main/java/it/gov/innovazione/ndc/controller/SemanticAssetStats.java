54725a68dee98713b67f770a38d3c85bdd4c5436
ndc-dxc <162444006+ndc-dxc@users.noreply.github.com>
2024-11-24 19:19:46 +0100

    Req tracing #2 (#150)
    * harvester will stop for fatal errors
    
    * adds accepts header for url check
    
    * change stat response

--- src/main/java/it/gov/innovazione/ndc/controller/SemanticAssetStats.java
+++ src/main/java/it/gov/innovazione/ndc/controller/SemanticAssetStats.java
@@ -9,16 +9,17 @@ import java.math.RoundingMode;
 @Data
 @Builder
 public class SemanticAssetStats {
-    private final SemanticAssetTypeStats totalStats;
-    private final SemanticAssetTypeStats controlledVocabularyStats;
-    private final SemanticAssetTypeStats ontologyStats;
-    private final SemanticAssetTypeStats schemaStats;
+    private final SemanticAssetTypeStats total;
+    private final SemanticAssetTypeStats controlledVocabulary;
+    private final SemanticAssetTypeStats ontology;
+    private final SemanticAssetTypeStats schema;
 
     @Data
     @Builder
     public static class SemanticAssetTypeStats {
         private final long current;
         private final long lastYear;
+        private final StatusStat status;
 
         public long getIncrementOverLastYear() {
             return current - lastYear;
@@ -31,5 +32,14 @@ public class SemanticAssetStats {
                             : ((double) current - lastYear) / lastYear * 100);
             return bigDecimal.setScale(1, RoundingMode.HALF_UP).doubleValue();
         }
+    }
+
+    @Data
+    @Builder
+    public static class StatusStat {
+        private final double archived;
+        private final double published;
+        private final double closedAccess;
+        private final double draft;
     }
 }
